f8020adcbf6ee2939c6fd61b2606745d9b4cafaf
Shanu Salunke <shanu@gatech.edu>
2015-11-27 22:35:27 -0500

    structures

--- rvm_internal.h
+++ rvm_internal.h
@@ -1,6 +1,15 @@
+#include <map>
 #include <string>
 using std::string;
 
+typedef struct seg_t{
+  std::string   seg_name;
+  int           size;
+  bool          is_mapped;
+  bool          is_about_to_modify;
+}seg_t;
+
 typedef struct rvm_t{
-  std::string store_dir;
+  std::string                   store_dir;
+  std::map<std::string, seg_t>  seg_map;
 }rvm_t;
