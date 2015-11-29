aecc3cea250fe3cc8673567e66376a34c262634a
Shanu Salunke <shanu@gatech.edu>
2015-11-28 20:47:59 -0500

    syncing

--- rvm_internal.h
+++ rvm_internal.h
@@ -1,16 +1,43 @@
 #include <map>
 #include <string>
-using std::string;
+using namespace std;
 
-typedef struct seg_t{
-  std::string   seg_nam  ;
+typedef struct segment{
+  string   seg_name  ;
   int           size;
   bool          is_mapped;
   bool          in_use;
   void*         address;
+
+  segment()
+{
+  is_mapped = 0;
+  in_use = 0;
+};
+segment(void *seg_address, int seg_size)
+{
+  address = seg_address;
+  size = seg_size;
+  is_mapped = 0;
+  in_use = 0;
+};
 }seg_t;
 
-typedef struct rvm_t{
-  std::string                   store_dir;
-  std::map<std::string, seg_t>  seg_map;
-}rvm_t;
+typedef struct rvm{
+  string store_dir;
+  map<string, seg_t> seg_map;
+
+  rvm(const char *directory)
+{
+  store_dir = directory;
+}
+}rvm_data;
+
+typedef rvm_data* rvm_t;
+
+typedef int trans_t;
+
+typedef struct transaction_data {
+  rvm_t rvm;
+  map<void*, seg_t*> segments;
+} trans_data;
