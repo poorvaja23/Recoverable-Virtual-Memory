8bad1dd0456fdfa5c8e57084c5f42ba0433d991a
Shanu Salunke <shanu@gatech.edu>
2015-11-28 20:55:19 -0500

    temp

--- rvm.cpp
+++ rvm.cpp
@@ -246,5 +246,5 @@ trans_t rvm_begin_trans(rvm_t rvm, int numsegs, void **segbases)
 }
 
 void rvm_about_to_modify(trans_t tid, void *segbase, int offset, int size){
-
+  
 }
--- rvm_internal.h
+++ rvm_internal.h
@@ -8,35 +8,31 @@ typedef struct segment{
   bool          is_mapped;
   bool          in_use;
   void*         address;
-
   segment()
-{
-  is_mapped = 0;
-  in_use = 0;
-};
-segment(void *seg_address, int seg_size)
-{
-  address = seg_address;
-  size = seg_size;
-  is_mapped = 0;
-  in_use = 0;
-};
+  {
+    is_mapped = 0;
+    in_use = 0;
+  };
+  segment(void *seg_address, int seg_size)
+  {
+    address = seg_address;
+    size = seg_size;
+    is_mapped = 0;
+    in_use = 0;
+  };
 }seg_t;
 
 typedef struct rvm{
   string store_dir;
   map<string, seg_t> seg_map;
-
   rvm(const char *directory)
-{
-  store_dir = directory;
-}
+  {
+    store_dir = directory;
+  }
 }rvm_data;
 
 typedef rvm_data* rvm_t;
-
 typedef int trans_t;
-
 typedef struct transaction_data {
   rvm_t rvm;
   map<void*, seg_t*> segments;
